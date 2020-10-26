ee95372804ae5f857d714dc5ebf4d8b04fc09adf
n8mcdunna <n8.mcdonough@gmail.com>
2020-10-26 15:44:59 -0400

    Update pipfiles and example module

---
+++ lambdata/example_module.py
@@ -0,0 +1,12 @@
+'''Lamdata - collection of data science functions'''
+
+# access libraries through pipenv
+import pandas as pd
+import numpy as np
+
+FAVORITE_NUMS = [9, 14, 43]
+
+def df_cleaner(df):
+    """Cleans a df"""
+    # TO DO - implement function
+    pass
